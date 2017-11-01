5b5326f0ff0c85c6802cc503caae41f27335326b
frank-ec <31932689+frank-ec@users.noreply.github.com>
2017-11-01 08:45:05 -0500

    Update datos_conexion.php

--- config/datos_conexion.php
+++ config/datos_conexion.php
@@ -2,7 +2,7 @@
 <?php
 try{
 		
-	$base=new PDO("mysql:host=localhost; dbname=triaje" , "root", "3201442");
+	$base=new PDO("mysql:host=localhost; dbname=triaje" , "root", "");
 	
 	$base->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 	$base->exec("SET CHARACTER SET UTF8");
